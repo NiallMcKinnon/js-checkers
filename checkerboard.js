324fffdddc2e8757dd07090f65b906e52bc493a5
Niall McKinnon <nrm3009@uncw.edu>
2023-03-19 01:54:25 -0400

    Made board square a seperate class

--- checkerboard.js
+++ checkerboard.js
@@ -1,38 +1,42 @@
-class Checkerboard {
-    constructor(rows, cols, containerId, squareSize) {
-        this.rows = rows;
-        this.cols = cols;
-        this.container = document.getElementById(containerId);
-        this.squareSize = squareSize;
-        this.colors = ["white", "black"];
-        this.generateBoard();
-        this.container.addEventListener(
-            "click",
-            this.handleSquareClick.bind(this)
-        );
+class Square {
+    constructor(row, col, size, color) {
+        this.row = row;
+        this.col = col;
+        this.size = size;
+        this.color = color;
+        this.element = document.createElement("div");
+        this.element.classList.add("square");
+        this.element.style.width = `${size}px`;
+        this.element.style.height = `${size}px`;
+        this.element.style.backgroundColor = color;
+        this.element.addEventListener("click", this.handleClick.bind(this));
     }
 
-    generateBoard() {
-        for (let row = 0; row < this.rows; row++) {
-            for (let col = 0; col < this.cols; col++) {
-                const square = document.createElement("div");
-                square.style.width = `${this.squareSize}px`;
-                square.style.height = `${this.squareSize}px`;
-                square.classList.add(this.colors[(row + col) % 2]);
-                square.dataset.row = row;
-                square.dataset.col = col;
-                this.container.appendChild(square);
-            }
-        }
-        this.container.style.width = `${this.cols * this.squareSize}px`;
-        this.container.style.height = `${this.rows * this.squareSize}px`;
-    }
-
-    handleSquareClick(event) {
-        const square = event.target;
-        square.classList.toggle("white");
-        square.classList.toggle("black");
+    handleClick() {
+        this.color = (this.color == 'white') ? 'black' : 'white';
+        this.element.style.backgroundColor = this.color;
     }
 }
 
-//   const checkerboard = new Checkerboard(8, 8, "checkerboard-container", 40);
+class Checkerboard {
+    constructor(rows, cols, squareSize) {
+        this.rows = rows;
+        this.cols = cols;
+        this.squareSize = squareSize;
+        this.squares = [];
+        this.container = document.getElementById("checkerboard-container");
+        this.init();
+    }
+
+    init() {
+        const colors = ["black", "white"];
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                const color = colors[(i + j) % 2];
+                const square = new Square(i, j, this.squareSize, color);
+                this.squares.push(square);
+                this.container.appendChild(square.element);
+            }
+        }
+    }
+}
